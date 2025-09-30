ae066ca1fc0b7e829c6d4168c20bc9dece0f82e4
Adam Amster <adam.amster@alleninstitute.org>
2025-09-30 07:32:43 -0400

    initial

---
+++ src/deep_ccf_registration/datasets/slice_dataset.py
@@ -0,0 +1,186 @@
+import time
+from enum import Enum
+from pathlib import Path
+from typing import Optional
+
+import ants
+import numpy as np
+import pandas as pd
+import tensorstore
+from aind_smartspim_transform_utils.CoordinateTransform import CoordinateTransform
+from aind_smartspim_transform_utils.utils.utils import AcquisitionAxis, AcquisitionDirection, \
+    apply_transforms_to_points, convert_from_ants_space
+from loguru import logger
+from pydantic import BaseModel
+from scipy.ndimage import map_coordinates
+from torch.utils.data import Dataset
+
+class SliceOrientation(Enum):
+    SAGITTAL = 'sagittal'
+    CORONAL = 'coronal'
+    HORIZONTAL = 'horizontal'
+
+
+class ExperimentMetadata(BaseModel):
+    experiment_id: str
+    stitched_volume_path: Path
+    axes: list[AcquisitionAxis]
+    registered_shape: tuple[int, int, int]
+    registered_resolution: tuple[float, float, float]
+    ls_to_template_affine_matrix_path: Path
+    ls_to_template_inverse_warp_path: Path
+
+def _create_coordinate_dataframe(height: int, width: int, fixed_index_value: int) -> pd.DataFrame:
+    """
+    Create a DataFrame with all pixel coordinates from a slice.
+
+    Parameters:
+    height: Height of the image (y dimension)
+    width: Width of the image (x dimension)
+    fixed_index_value: Fixed index value
+
+    Returns:
+    pd.DataFrame: DataFrame with columns [z, y, x]
+    """
+    # Create coordinate arrays
+    y_coords, x_coords = np.meshgrid(np.arange(height), np.arange(width), indexing='ij')
+
+    # Flatten the coordinate arrays
+    y_flat = y_coords.flatten()
+    x_flat = x_coords.flatten()
+
+    z_flat = np.full(len(y_flat), fixed_index_value)
+
+    df = pd.DataFrame({
+        'z': z_flat,
+        'y': y_flat,
+        'x': x_flat
+    }).astype(float)
+
+    return df
+
+def _apply_transforms_to_points(points: np.ndarray, coord_transform: CoordinateTransform, experiment_meta: ExperimentMetadata, warp: np.ndarray):
+    affine_transformed_points = apply_transforms_to_points(
+        ants_pts=points,
+        transforms=[str(experiment_meta.ls_to_template_affine_matrix_path)],
+        invert=(True,)
+    )
+    displacements = np.zeros((len(affine_transformed_points), 3))
+
+    for component in range(3):
+        displacements[:, component] = map_coordinates(
+            warp[:, :, :, component],
+            affine_transformed_points.T,
+            order=1,
+            mode='nearest',
+            prefilter=False
+        )
+
+    transformed_points = affine_transformed_points - displacements
+
+    transformed_points = convert_from_ants_space(
+        coord_transform.ls_template_info, transformed_points
+    )
+
+    transformed_df = pd.DataFrame(
+        transformed_points, columns=["ML", "AP", "DV"]
+    )
+    return transformed_df
+
+class SliceDataset(Dataset):
+    def __init__(self, dataset_meta: list[ExperimentMetadata], ls_template_path: Path, orientation: Optional[SliceOrientation] = None, registration_downsample_factor: int = 3):
+        super().__init__()
+        self._dataset_meta = dataset_meta
+        self._orientation = orientation
+        self._registration_downsample_factor = registration_downsample_factor
+        self._warps = self._load_warps()
+
+        logger.info('Loading light sheet template')
+        self._ls_template = ants.image_read(str(ls_template_path))
+
+    def _load_warps(self) -> list[np.ndarray]:
+        logger.info('Loading warps')
+
+        warps = []
+        for experiment_meta in self._dataset_meta:
+            warp = ants.image_read(str(experiment_meta.ls_to_template_inverse_warp_path))
+            warp = warp.numpy()
+            warps.append(warp)
+        return warps
+
+    def _get_slice_from_idx(self, idx: int) -> tuple[int, int]:
+        num_slices = [x.registered_shape[self._get_slice_axis(axes=x.axes).dimension] for x in self._dataset_meta]
+        num_slices_cumsum = np.cumsum([0] + num_slices)
+        dataset_idx = int(np.searchsorted(num_slices_cumsum[1:], idx, side='right'))
+        slice_idx = int(idx - num_slices_cumsum[dataset_idx])
+        return dataset_idx, slice_idx
+
+    def _get_slice_axis(self, axes: list[AcquisitionAxis]):
+        if self._orientation == SliceOrientation.SAGITTAL:
+            slice_axis = [i for i in range(len(axes)) if axes[i].direction in (AcquisitionDirection.LEFT_TO_RIGHT, AcquisitionDirection.RIGHT_TO_LEFT)]
+            if len(slice_axis) != 1:
+                raise ValueError(f'expected to find 1 sagittal axis but found {len(slice_axis)}')
+            slice_axis = axes[slice_axis[0]]
+        else:
+            raise NotImplementedError(f'{self._orientation} not supported')
+        return slice_axis
+
+    def __getitem__(self, idx):
+        dataset_idx, slice_idx = self._get_slice_from_idx(idx=idx)
+        experiment_meta = self._dataset_meta[dataset_idx]
+        acquisition_axes = experiment_meta.axes
+
+        slice_axis = self._get_slice_axis(axes=acquisition_axes)
+        height = experiment_meta.registered_shape[[x.dimension for x in acquisition_axes if x.name != slice_axis.name][0]]
+        width = experiment_meta.registered_shape[[x.dimension for x in acquisition_axes if x.name != slice_axis.name][1]]
+
+        point_grid = _create_coordinate_dataframe(
+            height=height,
+            width=width,
+            fixed_index_value=slice_idx
+        )
+
+        coord_transform = CoordinateTransform(
+            name='smartspim_lca',
+            dataset_transforms={
+                'points_to_ccf': [
+                    str(experiment_meta.ls_to_template_affine_matrix_path),
+                    str(experiment_meta.ls_to_template_inverse_warp_path),
+                ]
+            },
+            acquisition_axes=acquisition_axes,
+            image_metadata={'shape': experiment_meta.registered_shape},
+            ls_template=self._ls_template
+        )
+
+        points = coord_transform.prepare_points_for_forward_transform(
+            points=point_grid,
+            points_resolution=list(experiment_meta.registered_resolution)
+        )
+
+        ls_template_points = _apply_transforms_to_points(
+            points=points,
+            coord_transform=coord_transform,
+            experiment_meta=experiment_meta,
+            warp=self._warps[dataset_idx]
+        )
+
+
+        volume_slice = [0, 0, slice(None), slice(None), slice(None)]
+        volume_slice[slice_axis.dimension + 2] = slice_idx
+
+        volume = tensorstore.open(
+            spec={
+                'driver': 'file',
+                'path': str(experiment_meta.stitched_volume_path / str(self._registration_downsample_factor))
+            },
+            read=True
+        ).result()
+        input_slice = volume[tuple(volume_slice)].read().result()
+
+        output_points = ls_template_points.values.reshape((height, width, 3))
+        return input_slice, output_points
+
+    def __len__(self):
+        num_slices = [x.registered_shape[self._get_slice_axis(axes=x.axes).dimension] for x in self._dataset_meta]
+        return len(self._dataset_meta) * sum(num_slices)
---
+++ src/deep_ccf_registration/utils/utils.py
@@ -0,0 +1,81 @@
+import ants
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from skimage.exposure import rescale_intensity
+
+
+def visualize_alignment(input_slice: np.ndarray, template_points: pd.DataFrame, template: ants.ANTsImage):
+    raw_rgb = np.zeros_like(input_slice, shape=(*input_slice.shape, 3))
+    raw_rgb[:, :, 0] = input_slice
+
+    template_on_input = sample_template_at_points(
+        template=template,
+        template_points=template_points
+    )
+    template_on_input_rgb = np.zeros_like(template_on_input,
+                                             shape=(*template_on_input.shape, 3))
+    template_on_input_rgb[:, :, 2] = template_on_input_rgb
+
+    fig, ax = plt.subplots(figsize=(10, 10), ncols=3)
+    ax[0].imshow(rescale_intensity(raw_rgb, out_range=(0, 1)), alpha=0.8)
+    ax[0].imshow(rescale_intensity(template_on_input_rgb, out_range=(0, 1)), alpha=0.4)
+    ax[1].imshow(input_slice, cmap='gray')
+    ax[1].set_title('input')
+    ax[2].imshow(template_on_input, cmap='gray')
+    ax[2].set_title('Template')
+    return fig
+
+def create_3d_plot(points: pd.DataFrame, space: ants.ANTsImage) -> plt.Figure:
+    points = points[(points >= 0).all(axis=1)]
+    points = points[(points.loc[:, 'ML'] <= space.shape[0]) & (points.loc[:, 'AP'] <= space.shape[1]) & (points.loc[:, 'DV'] <= space.shape[2])]
+
+    ap_coords = points['AP']
+    dv_coords = points['DV']
+    ml_coords = points['ML']
+
+    fig = plt.figure(figsize=(15, 10))
+
+    # Main 3D plot - ML view to see slice angle
+    ax_3d = fig.add_subplot(projection='3d')
+
+    ax_3d.scatter(ap_coords, ml_coords, dv_coords,
+                           alpha=0.8, s=30, edgecolors='black', linewidth=0.2)
+
+    ax_3d.set_ylim(0, space.shape[0])
+
+    # Labels for ML view
+    ax_3d.set_xlabel('Anterior ← → Posterior', fontsize=11, fontweight='bold')
+    ax_3d.set_ylabel('Right ← → Left', fontsize=11, fontweight='bold')
+    ax_3d.set_zlabel('Superior ← → Inferior', fontsize=11, fontweight='bold')
+    ax_3d.set_title('Sagittal slice in LS template space', fontsize=13, fontweight='bold')
+
+    ax_3d.view_init(elev=30, azim=30)
+
+    plt.tight_layout()
+    return fig
+
+def sample_template_at_points(template_points: pd.DataFrame, template: ants.ANTsImage, interpolation='nearest') -> np.ndarray:
+    template_points = template_points.values
+
+    # Check bounds
+    valid_mask = (
+            (template_points[:, 0] >= 0) & (template_points[:, 0] < template.shape[0]) &
+            (template_points[:, 1] >= 0) & (template_points[:, 1] < template.shape[1]) &
+            (template_points[:, 2] >= 0) & (template_points[:, 2] < template.shape[2])
+    )
+
+    values = np.zeros(len(template_points))
+
+    if interpolation == 'nearest':
+        # Round to nearest integer
+        template_points = np.floor(template_points[valid_mask]).astype(int)
+        values[valid_mask] = template[
+            template_points[:, 0],
+            template_points[:, 1],
+            template_points[:, 2]
+        ]
+    else:
+        raise NotImplementedError
+
+    return values
