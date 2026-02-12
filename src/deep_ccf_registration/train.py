460881a0544b89f4ee911d90e94b2c160fda4108
Adam Amster <adam.amster@alleninstitute.org>
2026-02-12 17:13:06 -0500

    fix reducelr patience

--- src/deep_ccf_registration/train.py
+++ src/deep_ccf_registration/train.py
@@ -473,7 +473,7 @@ def train(
 
     # Setup learning rate scheduler
     if lr_scheduler == LRScheduler.ReduceLROnPlateau:
-        main_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, patience=100)
+        main_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, patience=patience)
         if scheduler_state_dict is not None:
             main_scheduler.load_state_dict(scheduler_state_dict)
             logger.info("Restored scheduler state from checkpoint")
